c2e30a17e479d07cfb971d9564f35c38bc813ce8
Shonkor Das <94380595+shonkor-das@users.noreply.github.com>
2022-12-05 00:05:12 +0600

    login from create

--- src/Contexts/AuthProvider.js
+++ src/Contexts/AuthProvider.js
@@ -8,8 +8,10 @@ const auth = getAuth(app)
 
 const AuthProvider = ({children}) => {
     const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     const createUser = (email, password) =>{
+        setLoading(true);
         return createUserWithEmailAndPassword(auth, email, password);
     }
 
@@ -29,6 +31,7 @@ const AuthProvider = ({children}) => {
         const unsubscribe = onAuthStateChanged(auth, currentUser =>{
             console.log('User observing');
             setUser(currentUser);
+            setLoading(false);
         });
         return() => unsubscribe();
     }, [])
