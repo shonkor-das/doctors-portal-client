6cab905ca996beb27856a4644e87494088c8ca39
Shonkor Das <94380595+shonkor-das@users.noreply.github.com>
2022-12-04 23:55:16 +0600

    testimonial section added

--- src/Routes/PrivateRoute/PrivateRoute.js
+++ src/Routes/PrivateRoute/PrivateRoute.js
@@ -1,14 +1,15 @@
 import React, {useContext} from 'react';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 import { AuthContext } from '../../Contexts/AuthProvider';
 
 const PrivateRoute = ({children}) => {
   const {user} = useContext(AuthContext);
+  const location = useLocation();
 
   if(user){
     return children;
   }
-  return <Navigate to="/login"></Navigate>
+  return <Navigate to="/login" state={{from: location}} replace></Navigate>
 };
 
 export default PrivateRoute;
